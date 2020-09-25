0c3b57ab5d2f02e7b395c328db900fca6ced2b3f
WangNingkai <1655586865@qq.com>
2020-09-25 16:54:43 +0800

    fix db

--- app/controller/test.js
+++ app/controller/test.js
@@ -5,21 +5,22 @@ class TestController extends Controller {
   async index() {
     const {ctx, app} = this
     try {
-      //   await app.model.authenticate()
-      //   const name = 'admin'
-      //   const password = '123456'
-      //   const user = await app.model.User.create({
-      //     name,
-      //     password,
-      //     status: 1,
-      //     is_admin: 1,
-      //   })
-      //   user.save()
-      const setting = await app.model.Setting.create({
-        name: 'app',
-        value: 'olaindex',
+      await app.model.authenticate()
+      const username = 'admin'
+      const password = ctx.helper.hash('123456')
+      const user = await app.model.User.create({
+        username,
+        password,
+        status: 1,
+        is_admin: 1,
       })
-      setting.save()
+      user.save()
+      //   const setting = await app.model.Setting.create({
+      //     name: 'app',
+      //     value: 'olaindex',
+      //   })
+      //   setting.save()
+      ctx.body = user.toJSON()
     } catch (error) {
       ctx.logger.error(error)
       ctx.body = ctx.helper.renderError(error.code)
